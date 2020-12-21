3f6210895aaabfc91f44d3f011a960f4fd920860
Stepan Drogin <drogenko.stepan@yandex.ru>
2020-12-21 21:58:26 +0300

    added layout for vue routing

---
+++ src/components/src/page-layout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="page__layout">
+        <lap-header/>
+        <keep-alive>
+        <router-view></router-view>
+        </keep-alive>
+        <lap-footer/>
+    </div>
+</template>
+
+<script>
+import LapFooter from './lap-footer.vue'
+import lapHeader from './lap-header.vue'
+
+export default {
+  components: { lapHeader,LapFooter,},
+    name: 'page-layout'
+}
+</script>
