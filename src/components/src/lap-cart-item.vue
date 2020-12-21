5155d61d098a7ec270dd94ed39e4c7549773b343
Stepan Drogin <drogenko.stepan@yandex.ru>
2020-12-21 21:55:41 +0300

    added cart item

---
+++ src/components/src/lap-cart-item.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="cart__item">
+        <div class="lap__container">
+            <div class="cart__container">
+            <div class="cart__item-img">
+                <img class="lap__part-img" :src="require('../../assets/lap-card/' + cart_item_data.image)" alt="auto-part" />
+            </div>
+            <p class="lap__part-name">{{cart_item_data.name}}</p>
+            <div class="cart__counter">
+                <button class="cart__counter-decrement" @click="decrementItem">-</button>
+                <p class="cart__counter-num">{{cart_item_data.quantity}}</p>
+                <button class="cart__counter-increment" @click="incrementItem">+</button>
+            </div>
+            <div class="cart__price-and-delete">
+                <p class="cart__item-price">{{cart_item_data.price}}</p>
+                <button class="cart__item-delete" @click="deleteFromCart">&#128465;&#65039;</button>
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'lap-cart-item',
+    props: {
+        cart_item_data: {
+            type: Object,
+            default() {
+                return {}
+            }
+        }
+    },
+    data() {
+        return {}
+    },
+        mounted() {
+        this.$set(this.cart_item_data, 'quantity', 1)
+    },
+    methods: {
+        deleteFromCart() {
+           this.$emit('deleteFromCart')
+        },
+        decrementItem() {
+            this.$emit('decrement')
+        },
+        incrementItem() {
+            this.$emit('increment')
+        }
+    }
+}
+</script>
+
+<style>
+.cart__container {
+    display: flex;
+    border-top: 1px solid #383838;
+    border-bottom: 1px solid #383838;
+    margin: 35px 0px;
+}
+.cart__counter {
+    display: flex;
+    margin: 25px;
+    align-items: center;
+    
+}
+.lap__part-name {
+    margin: auto;
+}
+.cart__counter-num {
+padding: 0px 20px;
+}
+.cart__price-and-delete {
+    display: flex;
+    align-items: center;
+}
+.cart__item-price {
+    margin: 0px 15px;
+}
+</style>
