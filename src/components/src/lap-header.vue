c07a90da2bdeb564c2ae467a453d808dc214bbad
Stepan Drogin <drogenko.stepan@yandex.ru>
2020-12-21 21:56:16 +0300

    united with header

--- src/components/src/lap-header.vue
+++ src/components/src/lap-header.vue
@@ -1,4 +1,5 @@
 <template>
+ <div class="header">
     <div class="header__info">
         <div class="header__info-phone">
         <img src="../../assets/img-header/phone-call.svg" alt="phone-call">
@@ -12,16 +13,82 @@
          <img src="../../assets/img-header/time-icon.svg" alt="time-icon">
          <p class="header__info">Мы открыты с 9.00 - 17.00</p>
         </div>
+        </div>
+            <div class="nav__menu">
+        <div class="nav__logo">
+        <img class="nav__logo" src="../../assets/img-header/shop-logo.svg" alt="shop-logo">
+        <p class="nav__name">Largus Auto Parts</p></div>
+        <div class="nav__menu-items">
+            <p class="nav__menu-item">Главная</p>
+            <p class="nav__menu-item">Каталог</p>
+            <p class="nav__menu-item">Контакты</p>
+            <p class="nav__menu-item">Прочее</p>
+            <router-link :to="{name: 'cart', params: {cart_data: CART}}">
+            <div class="cart__wrapper">
+            <img class="nav__menu-cart" src="../../assets/img-header/shop-cart.svg" alt="shop-cart">
+            <p class="cart__count">{{CART.length}}</p>
+            </div>
+            </router-link>
+            <img class="nav__menu-search" src="../../assets/img-header/shop-search.svg" alt="shop-search">
+        </div>
+    </div>
     </div>
 </template>
 <script>
+import { mapGetters } from 'vuex'
 export default {
     name: 'lap-header',
+    computed: {
+        ...mapGetters([
+            'CART'
+        ])
+    }
 }
 </script>
 <style>
 body {
     margin: 0;
+}
+a {
+    text-decoration: none;
+    color: white;
+}
+.cart__wrapper {
+    display: flex;
+}
+.cart__count {
+    background: #E83C26;
+    border-radius: 64px;
+    width: 25px;
+    height: 20px;
+    text-align: center;
+    text-decoration: none;
+}
+.nav__menu {
+    display: flex;
+    min-height: 173px;
+}
+.nav__name {
+    margin: auto;
+    max-width: 100px;
+}
+.nav__logo {
+    display: flex;
+    margin: auto;
+}
+.nav__menu-items {
+    display: flex;
+    justify-content: space-around;
+    min-width: 50%;
+    margin: auto;
+}
+.nav__menu-item {
+    text-decoration: none;
+    display: flex;
+}
+.nav__menu-list {
+    display: flex;
+    justify-content: space-between;
 }
 .header__info {
     display: flex;
