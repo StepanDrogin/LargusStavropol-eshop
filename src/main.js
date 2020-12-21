c1e0b3f99a96daf9d97ce345c7c9ec45820c59a2
Stepan Drogin <drogenko.stepan@yandex.ru>
2020-12-21 21:54:22 +0300

    import/render store and router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './components/vuex/store'
+import router from './components/router/router'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
+  store
 }).$mount('#app')
