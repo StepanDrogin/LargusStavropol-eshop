147267fe9baa9b2101f36e4e91949b4d13c629b2
Stepan Drogin <drogenko.stepan@yandex.ru>
2020-12-21 21:53:48 +0300

    added vue routing

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,15 @@
 <template>
   <div id="app">
-    <lap-header/>
-    <lap-nav/>
-    <lap-main-wrapper/>
-    <lap-footer/>
-  </div>
+    <keep-alive>
+    <router-view></router-view>  
+    </keep-alive>
+    </div>
 </template>
 
 <script>
-import LapMainWrapper from './components/lap-main-wrapper.vue'
-import LapFooter from './components/src/lap-footer.vue'
-import LapHeader from './components/src/lap-header'
-import LapNav from './components/src/lap-nav.vue'
 export default {
   name: 'App',
-  components: {
-    LapHeader,
-    LapNav,
-    LapMainWrapper,
-    LapFooter,
-  }
+  components: {}
 }
 </script>
 
