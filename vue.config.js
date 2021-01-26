8d13eecd214cca8629a33b991902e827412431d6
Stepan Drogin <drogenko.stepan@yandex.ru>
2021-01-26 13:55:07 +0300

    no change

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ module.exports = {
     css: {
         loaderOptions: {
             sass: {
-                additionalData: `@import "@/assets/styles/variables.scss";`
+                additionalData: `@import "@/assets/styles/variables.scss";`,
             },
         },
-    }
+    },
 };
