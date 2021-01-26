64dd75416358b2a0ca08a19a4aecd0144a996b61
Stepan Drogin <drogenko.stepan@yandex.ru>
2021-01-26 13:55:29 +0300

    imported new plugins

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './components/vuex/store'
 import router from './components/router/router'
+import 'material-design-icons-iconfont'
+import VueScrollProgress from 'vue-scroll-progress'
+Vue.use(VueScrollProgress)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
   router,
-  store
+  store,
 }).$mount('#app')
